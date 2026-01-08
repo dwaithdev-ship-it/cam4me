0e85d7718d21cebd24b754258704732bf3c001c8
Kalyan <dwaith.dev@gmail.com>
2026-01-08 17:20:34 +0530

    Fix SSL issues and add share script for localhost

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   server: {
     port: 5173,
     strictPort: false,
